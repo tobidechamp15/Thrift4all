e11b8aae4f7610f6f4e81df9ab3fd76d4a0f7143
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-05-03 00:24:30 +0100

    Feat-Search

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "./Navbar";
+import Search from "./Search";
 
 const AllProducts = () => (
   <>
     <Navbar />
+    <Search />
   </>
 );
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import filterIcon from "../assets/Filter.svg";
+import addIcon from "../assets/addIcon.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Search = () => {
+  return (
+    <div className="flex items-center mx-3 my-5">
+      <img src={filterIcon} alt="" />
+      <div className="flex w-full justify-center items-center relative">
+        <FontAwesomeIcon icon={faSearch} className="absolute left-[40px]" />
+        <input
+          type="search"
+          className="w-full mx-4 rounded-[50px] border-black border-2   ps-[45px] py-3 form-control outline-none"
+          placeholder="Search for products"
+          id=""
+        />
+      </div>
+      <img src={addIcon} className="cursor-pointer " />
+    </div>
+  );
+};
+
+export default Search;
