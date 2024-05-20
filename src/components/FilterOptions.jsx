050f8caf9ad56d86491bf57c0b0d238cf2715264
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-05-20 21:14:24 +0100

    New Arrival

--- src/components/FilterOptions.jsx
+++ src/components/FilterOptions.jsx
@@ -6,23 +6,25 @@ import arrow from "../assets/Arrow.svg";
 
 const FilterOptions = () => {
   return (
-    <div className="flex gap-2 w-ful">
-      <div className="flex flex-col gap-1 m-2 cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] rounded-[25px] justify-between border-1 border-[#B31430] w-[25%] items-center bg-[#B715321A] p-3">
-        <img src={clothIcon} className=" w-max" />
-        <p className="optionName">Clothing</p>
-      </div>
-      <div className="flex flex-col gap-1 m-2 justify-between cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] rounded-[25px] border-1 border-[#B31430] w-[25%] items-center bg-[#B715321A] p-3">
-        <img src={shoeIcon} className=" w-max" />
-        <p className="optionName">Shoe</p>
-      </div>
-      <div className="flex flex-col justify-between gap-1 m-2 cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] rounded-[25px] border-1 border-[#B31430] w-[25%] items-center bg-[#B715321A] p-3">
-        <img src={speakerIcon} className=" w-max" />
-        <p className="optionName">Speaker</p>
-      </div>
-      <div className="flex flex-col gap-1 m-2 cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] justify-between rounded-[25px] border-1 border-[#B31430] w-[25%] items-center bg-[#B715321A] p-3">
-        <span className=" font-normal text-xs font-[Pacifico]">View all</span>
-        <img src={arrow} alt="" />
-      </div>
+    <div className="w-full flex justify-center" >
+      <div className="flex gap-2 overflow- xsm:w-[100%] w-[70%] ">
+        <div className="flex flex-col gap-1 m-2 cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] rounded-[25px] justify-between border-1 border-[#B31430] w-[450px] h-[200px] items-center bg-[#B715321A] p-3">
+          <img src={clothIcon} className=" w-auto h-full" />
+          <p className="optionName">Clothing</p>
+        </div>
+        <div className="flex flex-col gap-1 m-2 justify-between cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] rounded-[25px] border-1 border-[#B31430] w-[450px] h-[200px] items-center bg-[#B715321A] p-3">
+          <img src={shoeIcon} className=" w-auto h-full" />
+          <p className="optionName">Shoe</p>
+        </div>
+        <div className="flex flex-col justify-between gap-1 m-2 cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] rounded-[25px] border-1 border-[#B31430] w-[450px] h-[200px] items-center bg-[#B715321A] p-3">
+          <img src={speakerIcon} className=" w-auto h-full" />
+          <p className="optionName">Speaker</p>
+        </div>
+        <div className="flex flex-col gap-1 m-2 cursor-pointer transition-all ease-in-out hover:bg-[#dddbdb] justify-between rounded-[25px] border-1 border-[#B31430] w-[450px] h-[200px] items-center bg-[#B715321A] p-3">
+          <span className=" font-normal text-xs font-[Pacifico]">View all</span>
+          <img src={arrow} alt="" />
+        </div>
+      </div>{" "}
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => (
-  <div className="flex justify-between m-2 my-4">
-    <span />
+  <div className="flex justify-around m-2 my-4 w-full ">
     <div className="logoStyle">Thrift4all</div>
-    <span />
-    <FontAwesomeIcon icon={faBars} size="2x" className="mx-[39px]" />
+    <div className="flex gap-7 items-center justify-between w-[40%] my-2">
+      <div className="flex gap-4">
+        <span className="nav-items">Home</span>
+        <span className="nav-items">Products</span>
+        <span className="nav-items">Add Products</span>
+        <span className="nav-items">Contact</span>
+      </div>
+      <div className="logout-btn">LOGOUT</div>
+    </div>
+    <FontAwesomeIcon
+      icon={faBars}
+      size="2x"
+      className="mx-[39px] block md:hidden"
+    />
   </div>
 );
 
