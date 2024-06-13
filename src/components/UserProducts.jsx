c16097faeb1661ed91b4e1ca5b8eb901047e0a10
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-06-13 06:33:53 +0100

    dev changes

--- src/components/UserProducts.jsx
+++ src/components/UserProducts.jsx
@@ -27,21 +27,39 @@ const UserProducts = () => {
   }, []); // empty dependency array to ensure the effect runs only once
 
   return (
-    <div>
+    <div className="container mx-auto p-4">
       {products.length > 0 ? (
-        products.map((product) => (
-          <div key={product.id}>
-            <h3>{product.productName}</h3>
-            <p>Price: {product.price}</p>
-            <p>Description: {product.productDescription}</p>
-            <a href={product.businessLink}>Business Page</a>
-            {product.imageSrc && (
-              <img src={product.imageSrc} alt={product.productName} />
-            )}
-          </div>
-        ))
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white hover:shadow-lg transition-shadow duration-500  hover:scale-105 ease-in-out rounded-lg overflow-hidden"
+            >
+              {product.imageSrc && (
+                <img
+                  src={product.imageSrc}
+                  alt={product.productName}
+                  className="w-full h-48 object-cover"
+                />
+              )}
+              <div className="p-4">
+                <h3 className="text-lg font-semibold">{product.productName}</h3>
+                <p className="text-gray-600">Price: ${product.price}</p>
+                <p className="text-gray-700 mt-2">
+                  {product.productDescription}
+                </p>
+                <a
+                  href={product.businessLink}
+                  className="text-blue-500 hover:underline mt-4 block"
+                >
+                  Business Page
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
       ) : (
-        <p>No products found.</p>
+        <p className="text-center text-gray-500">No products found.</p>
       )}
     </div>
   );
