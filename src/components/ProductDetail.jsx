0033c09ba249ced36b037700d568c9db670aacb8
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-07-12 00:00:55 +0100

    dev changes

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,11 +1,9 @@
 import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { ProductContext } from "./ProductContext";
-import backIcon from "../assets/backIcon.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
 const ProductDetail = () => {
   const { selectedProduct } = useContext(ProductContext);
@@ -20,43 +18,8 @@ const ProductDetail = () => {
   return (
     <>
       <div className="bg-gray-100 min-h-screen">
-        {/* Navigation */}
-        <nav className="bg-white shadow-md">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-              <div className="flex">
-                <Link to="/" className="flex-shrink-0 flex items-center">
-                  <img src={backIcon} className="h-8 w-8" alt="Back" />
-                </Link>
-              </div>
-              <div className="hidden md:flex items-center space-x-4">
-                <Link to="/" className="nav-item">
-                  Home
-                </Link>
-                <Link to="/user-products" className="nav-item">
-                  Products
-                </Link>
-                <Link to="/addproducts" className="nav-item">
-                  Add Products
-                </Link>
-                <span className="nav-item">Contact</span>
-                <Link to="/" className="logout-btn">
-                  LOGOUT
-                </Link>
-              </div>
-              <div className="md:hidden flex items-center">
-                <FontAwesomeIcon
-                  icon={faBars}
-                  size="lg"
-                  className="text-gray-500"
-                  onClick={toggleSidebar}
-                />
-              </div>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        {/* Product Details */}
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <h1 className="text-3xl font-semibold text-center mb-8 font-[Pacifico]">
             Product Details
